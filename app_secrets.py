dc64b44a3567568c8138cc41b91bb0262394de49
jcm10+1-at-391170303014 <john.connor.mulligan@gmail.com>
2022-04-22 10:40:10 -0500

    removed hard-coding on remote endpoint

--- app_secrets.py
+++ app_secrets.py
@@ -1,7 +1,6 @@
 import os
 
 voyages_auth_token=os.environ['voyages_auth_token']
+base_url=os.environ['base_url']
 
 headers={'Authorization': 'Token %s' %voyages_auth_token}
-
-base_url="http://127.0.0.1:8000/"
